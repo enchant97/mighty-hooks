255b1eec335f7fa41a389bfc79e7bf5c3fec4501
Leo Spratt <enchant97@users.noreply.github.com>
2023-02-13 20:41:00 +0000

    fix hex decode&encode

--- crates/core/src/signing.rs
+++ crates/core/src/signing.rs
@@ -6,16 +6,16 @@ pub fn sign_hmac_sha256(secret: &str, data: &[u8]) -> String {
     let key = hmac::Key::new(hmac::HMAC_SHA256, secret.as_bytes());
     let signature = hmac::sign(&key, data);
     let signature = signature.as_ref();
-    // Convert the signature to a hex string
-    signature
-        .iter()
-        .map(|b| format!("{:02x}", b))
-        .collect::<Vec<String>>()
-        .join("")
+    hex::encode(signature)
 }
 
 /// Verify a signed string with a secret using HMAC SHA256 and return true if it is valid
-pub fn verify_hmac_sha256(secret: &str, data: &[u8], signature: &str) -> bool {
-    let key = hmac::Key::new(hmac::HMAC_SHA256, secret.as_bytes());
-    hmac::verify(&key, data, &signature.as_bytes()).is_ok()
+pub fn verify_hmac_sha256(secret: &str, data: &[u8], signature_hex: &str) -> bool {
+    match hex::decode(signature_hex) {
+        Ok(signature) => {
+            let key = hmac::Key::new(hmac::HMAC_SHA256, secret.as_bytes());
+            hmac::verify(&key, data, &signature).is_ok()
+        }
+        Err(_) => return false,
+    }
 }
