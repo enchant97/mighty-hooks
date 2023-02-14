d700e36e259edd51dd5aaa7c58beb5195283f862
Leo Spratt <enchant97@users.noreply.github.com>
2023-02-14 22:48:52 +0000

    add unit test for hmac signing

--- crates/core/src/signing.rs
+++ crates/core/src/signing.rs
@@ -20,3 +20,31 @@ pub fn verify_hmac_sha256(secret: &str, data: &Bytes, signature_hex: &str) -> bo
         Err(_) => return false,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use bytes::Bytes;
+
+    use super::*;
+
+    #[test]
+    fn test_sign_hmac_sha256() {
+        let expected =
+            String::from("ffad4a016b7e758451f02a86a9c9b504be8e70c0df26afbab7dccc8ddeb90a71");
+        let result = super::sign_hmac_sha256("my-secret", &Bytes::from("my-data"));
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_verify_hmac_sha256() {
+        let signature =
+            String::from("ffad4a016b7e758451f02a86a9c9b504be8e70c0df26afbab7dccc8ddeb90a71");
+        let result_true = verify_hmac_sha256("my-secret", &Bytes::from("my-data"), &signature);
+        assert_eq!(true, result_true);
+        let result_false = verify_hmac_sha256("not-my-secret", &Bytes::from("my-data"), &signature);
+        assert_eq!(false, result_false);
+        let result_false_2 =
+            verify_hmac_sha256("my-secret", &Bytes::from("not-my-data"), &signature);
+        assert_eq!(false, result_false_2);
+    }
+}
