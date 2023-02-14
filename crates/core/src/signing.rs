7377c23e174b3b62ecf64fc055284b284830e774
Leo Spratt <enchant97@users.noreply.github.com>
2023-02-14 22:32:01 +0000

    accept Bytes instead of [u8]

--- crates/core/src/signing.rs
+++ crates/core/src/signing.rs
@@ -1,7 +1,8 @@
+use bytes::Bytes;
 use ring::hmac;
 
 /// Sign a string with a secret using HMAC SHA256 and return the signature as a hex string
-pub fn sign_hmac_sha256(secret: &str, data: &[u8]) -> String {
+pub fn sign_hmac_sha256(secret: &str, data: &Bytes) -> String {
     // Sign the data with the secret
     let key = hmac::Key::new(hmac::HMAC_SHA256, secret.as_bytes());
     let signature = hmac::sign(&key, data);
@@ -10,7 +11,7 @@ pub fn sign_hmac_sha256(secret: &str, data: &[u8]) -> String {
 }
 
 /// Verify a signed string with a secret using HMAC SHA256 and return true if it is valid
-pub fn verify_hmac_sha256(secret: &str, data: &[u8], signature_hex: &str) -> bool {
+pub fn verify_hmac_sha256(secret: &str, data: &Bytes, signature_hex: &str) -> bool {
     match hex::decode(signature_hex) {
         Ok(signature) => {
             let key = hmac::Key::new(hmac::HMAC_SHA256, secret.as_bytes());
